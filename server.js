6896f3e9dbbf564c3377ec4d0d03c3de36ddc3e3
Jack Hall-Tipping <jmht001@bucknell.edu>
2018-04-25 10:36:40 -0400

    working

---
+++ server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const bodyParser= require('body-parser')
+const app = express()
+
+app.use(bodyParser.urlencoded({extended: true}))
+
+const MongoClient = require('mongodb').MongoClient
+
+var db
+
+MongoClient.connect('mongodb://owner:tahFah7x@eg-mongodb/jmht001', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('jmht001') // whatever your database name is
+  app.listen(3000, () => {
+    console.log('listening on 3000')
+  })
+})
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+  // Note: __dirname is directory that contains the JavaScript source code. Try logging it and see what you get!
+  // Mine was '/Users/zellwk/Projects/demo-repos/crud-express-mongo' for this app.
+})
+
+app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
