bb4b77f4cdbf2211200f0f4811ba4870a16de7a0
Jack Hall-Tipping <jmht001@bucknell.edu>
2018-04-29 15:15:34 -0400

    fixing error

--- server.js
+++ server.js
@@ -59,7 +59,7 @@ app.post('/projects', (req, res) => {
 })
 
 
-app.post(‘/:pin’, (req, res) => {
+app.post('/:pin', (req, res) => {
   db.collection(‘projects’).update({pin:req.params}, {$set: {functions[req.body.index].proposed_solutions:req.body.solutions}});
 
-}
+})
