60b3bea9da3634f7ced2ee313a07bb11fbac4877
Jack Hall-Tipping <jmht001@bucknell.edu>
2018-04-29 14:36:06 -0400

    Adding readme and comments

---
+++ README.md
@@ -0,0 +1,14 @@
+# CrowdCode: Backend
+
+CrowdCode allows people from all over the world to code together.
+
+## Built With
+
+The backend was built by hosting a MongoDB database on an EC2 Amazon Web Server. We then used Node/Express to communicate with the database,
+and finally hosted the RESTful API using heroku.
+
+
+## Authors
+
+* **Jack Hall-Tipping**
+* **Jeff Lee**
--- server.js
+++ server.js
@@ -11,7 +11,15 @@ const MongoClient = require('mongodb').MongoClient
 
 var db = null
 
+//creates RESTful API
+//https://calm-headland-11311.herokuapp.com/
+//Above is the link
+//Endpoint is the pin used by /pin or an example https://calm-headland-11311.herokuapp.com//100001
+//if a pin exists it displays the json
+//otherwise it displays an error
+
 MongoClient.connect('mongodb://admin:password@18.191.29.110/projectDB', (err, client) => {
+  //MongoDB hosted on AWS
   if (err) return console.log(err)
   db = client.db('projectDB')
   app.listen(process.env.PORT || 3000, function(){
@@ -20,33 +28,18 @@ MongoClient.connect('mongodb://admin:password@18.191.29.110/projectDB', (err, cl
 })
 
 app.get('/', (req, res) => {
-  //return whole JSON database
+  //returns whole JSON MongoDB database
   db.collection('projects').find().toArray((err, result) => {
     if (err) return console.log(err)
     console.log(result)
-    //res.render('index.ejs', {projects: result})
     res.send(result)
   })
-  // db.collection("projects").findOne({"pin":"123"}, function(err, result) {
-  //   if (err) throw err;
-  //   console.log(result);
-  // });
-  //console.log(db.collection('projects').find())
 })
 
-// app.get('/:pin', (req, res) => {
-//   //get specific pin
-//   //console.log(db.collection('projects').find({"pin": "123"}))
-//   db.collection("projects").findOne({"pin":"123"}, function(err, result) {
-//     if (err) throw err
-//     console.log(result)
-//     res.send(result)
-//   });
-//   console.log('getting specific pin')
-// })
-
 app.get('/:pin', function (req, res) {
-  //res.send(req.params)
+  //returns a specific pin and the fields associated with it
+  //if the pin is not there, it returns a JSON object, result: error
+  //React checks for the error
   db.collection("projects").findOne(req.params, function(err, result) {
     if (err) throw err
     if (result == null)
@@ -57,9 +50,9 @@ app.get('/:pin', function (req, res) {
 })
 
 app.post('/projects', (req, res) => {
+  //used to post to the projects collection in the MongoDB
   db.collection('projects').save(req.body, (err, result) => {
     if (err) return console.log(err)
-
     console.log('saved to database')
     res.redirect('/')
   })
